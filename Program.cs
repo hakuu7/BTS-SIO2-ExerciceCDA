d4ecd5bda67c847669664b564a38c5cadb3b7369
hakuu7 <evan.monnerville@gmail.com>
2026-09-15 16:01:32 +0200

    Exercice 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿decimal solde = 1000m;
+decimal montant = -1150m;
+
+if (montant > 0)
+{solde += montant;
+}
+else if (montant <0)
+{
+    
+    decimal debit = -montant;
+    
+    if(debit <= solde)
+    {
+        solde -= debit;
+    }
+    else
+    {
+        Console.WriteLine("Solde insuffisant pour effectuer le retrait.");
+    }
+}
